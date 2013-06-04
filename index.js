30356cf15159e0479cd4ae5e77d8bbc947188a3b
Yaniv Kessler <yanivk@gmail.com>
2013-06-04 05:39:42 +0300

    added default server and such

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var config = require('./lib/config');
+var HttpTrafficRecorder = require('./lib/HttpTrafficRecorder');
 
-
+module.exports.config = require('./lib/config');
 
 module.exports.create = function(protocol, options) {
 	if (protocol === 'http')
-		return new HttpTrafficRecorder(options);
+		return new HttpTrafficRecorder(options || module.exports.config.store);
 
 	throw new Error('unsupported protocol ' + protocol);
-}
+};
